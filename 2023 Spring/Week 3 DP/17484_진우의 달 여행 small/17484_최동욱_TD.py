b9b69182dfbcbe1f25f9a0e62bb40db69a2e578a
Dwgygygy <dwchoi0610@gmail.com>
2023-03-26 21:52:38 +0900

    complete

--- 2023 Spring/Week 3 DP/17484_진우의 달 여행 small/17484_최동욱_TD.py
+++ 2023 Spring/Week 3 DP/17484_진우의 달 여행 small/17484_최동욱_TD.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10 ** 6) # 숫자는 예시
+
 memo = [[[] for _ in range(1000)] for _ in range(1000)]
 N, M = map(int, input().split())  # 2 <= N, M <= 6 / NxM matrix
 matrix = [[] for _ in range(N)]
@@ -5,140 +8,60 @@ matrix = [[] for _ in range(N)]
 for i in range(N):
     matrix[i] = list(map(int, input().split()))
 
-def dp(x, y):
+
+def dp(y, x):
     global memo
     global matrix
     global N, M
+
 
     if memo[y][x]:
         return memo[y][x]
 
     if y == 0:
-        return (matrix[0][x], 0)
+        return matrix[0][x]
 
     elif y == 1:
         if x == 0:
-            return ((dp[0][1][0] + matrix[1][0], 1), (dp[0][0][0] + matrix[1][0], 2))
-        elif x == M-1:
-            return ((dp[0][x][0] + matrix[1][x], 2), (dp[0][x-1][0] + matrix[1][x], 3))
+            memo[y][x] = dp(0, 1) + matrix[1][0], \
+                         dp(0, 0) + matrix[1][0], \
+                         9999999
+        elif x == M - 1:
+            memo[y][x] = 9999999, \
+                         dp(0, x) + matrix[1][x], \
+                         dp(0, x - 1) + matrix[1][x]
         else:
-            return ((dp[0][x+1][0] + matrix[1][x], 1), (dp[0][x][0] + matrix[1][x], 2), (dp[0][x+1][0] + matrix[1][x], 3))
+            memo[y][x] = dp(0, x + 1) + matrix[1][x], \
+                         dp(0, x) + matrix[1][x], \
+                         dp(0, x - 1) + matrix[1][x]
+
+        return memo[y][x]
 
     else:
+
         if x == 0:
-            return ((dp(x, y-1), dp(x+1, y-1)))
+            memo[y][x] = min(dp(y - 1, x + 1)[1], dp(y - 1, x + 1)[2]) + matrix[y][x], \
+                         dp(y - 1, x)[0] + matrix[y][x], \
+                         9999999
         elif x == M - 1:
-            return ((dp(x-1, y-1), dp(x, y-1)))
+            memo[y][x] = 9999999, \
+                         dp(y - 1, x)[2] + matrix[y][x], \
+                         min(dp(y - 1, x - 1)[0], dp(y - 1, x - 1)[1]) + matrix[y][x]
         else:
-            return ((dp(x-1, y-1), dp(x, y-1), dp(x+1, y-1))
+            memo[y][x] = min(dp(y - 1, x + 1)[1], dp(y - 1, x + 1)[2]) + matrix[y][x], \
+                         min(dp(y - 1, x)[0], dp(y - 1, x)[2]) + matrix[y][x],\
+                         min(dp(y - 1, x - 1)[0], dp(y - 1, x - 1)[1]) + matrix[y][x]
 
-
-    # dir -> 1: left, 2: direct, 3: right
-    for y in range(N):
-        for x in range(M):
-            if y == 0:
-                dp_table[y][x] = [(matrix[y][x], 0)]
-
-            elif y == 1:
-                current = matrix[1][x]
-                if x == 0:
-                    temp_up, temp_right = matrix[0][x], matrix[0][x + 1]
-                    dp_table[1][x] = [(temp_up + current, 2), (temp_right + current, 1)]
-
-                elif x == M - 1:
-                    temp_left, temp_up = matrix[0][x - 1], matrix[0][x]
-                    dp_table[1][x] = [(temp_left + current, 3), (temp_up + current, 2)]
-
-                else:
-                    temp_left, temp_up, temp_right = matrix[0][x - 1], matrix[0][x], matrix[0][x + 1]
-                    dp_table[1][x] = [(temp_left + current, 3), (temp_up + current, 2), (temp_right + current, 1)]
-
-            elif y >= 2:
-                current = matrix[y][x]
-                if x == 0:
-                    temp_up, temp_right = dp_table[y - 1][x], dp_table[y - 1][x + 1]
-                    min_up, min_right = (9999999, 0), (9999999, 0)
-
-                    for info in temp_up:
-                        if min_up[0] > info[0] + current:
-                            if info[1] == 2:
-                                continue
-                            else:
-                                min_up = (info[0] + current, 2)
-
-                    for info in temp_right:
-                        if min_right[0] > info[0] + current:
-                            if info[1] == 1:
-                                continue
-                            else:
-                                min_right = (info[0] + current, 1)
-
-                    dp_table[y][x] = [min_up, min_right]
-
-                elif x == M - 1:
-                    temp_left, temp_up = dp_table[y - 1][x - 1], dp_table[y - 1][x]
-                    min_left, min_up = (9999999, 0), (9999999, 0)
-
-                    for info in temp_left:
-                        if min_left[0] > info[0] + current:
-                            if info[1] == 3:
-                                continue
-                            else:
-                                min_left = (info[0] + current, 3)
-
-                    for info in temp_up:
-                        if min_up[0] > info[0] + current:
-                            if info[1] == 2:
-                                continue
-                            else:
-                                min_up = (info[0] + current, 2)
-
-                    dp_table[y][x] = [min_left, min_up]
-
-                else:
-                    temp_left, temp_up, temp_right = dp_table[y - 1][x - 1], dp_table[y - 1][x], dp_table[y - 1][x + 1]
-                    min_left, min_up, min_right = (9999999, 0), (9999999, 0), (9999999, 0)
-
-                    for info in temp_left:
-                        if min_left[0] > info[0] + current:
-                            if info[1] == 3:
-                                continue
-                            else:
-                                min_left = (info[0] + current, 3)
-
-                    for info in temp_up:
-                        if min_up[0] > info[0] + current:
-                            if info[1] == 2:
-                                continue
-                            else:
-                                min_up = (info[0] + current, 2)
-
-                    for info in temp_right:
-                        if min_right[0] > info[0] + current:
-                            if info[1] == 1:
-                                continue
-                            else:
-                                min_right = (info[0] + current, 1)
-
-                    dp_table[y][x] = [min_left, min_up, min_right]
-
-    min_route = 9999999
-    for routes in dp_table[N-1]:
-        for info in routes:
-            if min_route > info[0]:
-                min_route = info[0]
-
-    return min_route
+        return memo[y][x]
 
 
 if __name__ == "__main__":
-    N, M = map(int, input().split())  # 2 <= N, M <= 6 / NxM matrix
-    matrix = [[] for _ in range(N)]
+    ans_list = []
+    for x in range(M):
+        for v in dp(N-1, x):
+            ans_list.append(v)
 
-    for i in range(N):
-        matrix[i] = list(map(int, input().split()))
-
-    print(travel_to_moon(M, N, matrix))
+    print(min(ans_list))
 
 
 # 최대한 단계적으로 올라가야하는구나
