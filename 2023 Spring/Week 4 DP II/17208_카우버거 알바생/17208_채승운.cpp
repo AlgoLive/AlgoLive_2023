5c1de13ec12f4ecbc9f5d9d46f9417525d6647c9
CHAE <chaso1495@naver.com>
2023-03-27 23:45:12 +0900

    [feat] 17208 카우버거 알바생

--- 2023 Spring/Week 4 DP II/17208_카우버거 알바생/17208_채승운.cpp
+++ 2023 Spring/Week 4 DP II/17208_카우버거 알바생/17208_채승운.cpp
@@ -2,30 +2,33 @@
 #include <vector>
 
 using namespace std;
-int n, k, w, v;
-int dp[101][100001];
+int n, m, k;
+int x, y;
+int dp[101][301][301]; // 주문, 치즈, 감자
 vector<pair<int, int> > vec;
 
 int main(){
-    cin >> n >> k;
+    cin >> n >> m >> k; // 주문, 잔여 치즈 및 감자
     for(int i=0; i<n; i++){
-        cin >> w >> v;
-        vec.push_back(make_pair(w, v));
-    } // 무게와 가치 입력
+        cin >> x >> y;
+        vec.push_back(make_pair(x, y));
+    } // 치즈/감자 주문내역
 
     for(int i=1; i<=n; i++){
-        int weight = vec[i-1].first;
-        int val = vec[i-1].second;
+        int cheese = vec[i-1].first;
+        int potato = vec[i-1].second;
     
-        for(int j=0; j<=k; j++){
-            if(weight <= j){
-                dp[i][j] = max(dp[i-1][j-weight] + val, dp[i-1][j]);
-            } else {
-                dp[i][j] = dp[i-1][j];
+        for(int j=0; j<=m; j++){
+            for(int a=0; a<=k; a++){
+                if(cheese <= j && potato <= a){ // 둘 다 잔여량이 존재할 때만 주문처리가 가능하다.
+                    dp[i][j][a] = max(dp[i-1][j-cheese][a-potato] + 1, dp[i-1][j][a]);
+                } else {
+                    dp[i][j][a] = dp[i-1][j][a];
+                }
             }
         }
     }
-    
-    cout << dp[n][k] << '\n';
+
+    cout << dp[n][m][k] << '\n';
     return 0;
 }
