a6a164b7e37adf394cf172e346ab601282ffa3a0
CHAE <chaso1495@naver.com>
2023-03-27 23:57:46 +0900

    [feat] 9251 LCS

--- 2023 Spring/Week 4 DP II/9251_LCS/9251_채승운.cpp
+++ 2023 Spring/Week 4 DP II/9251_LCS/9251_채승운.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
 using namespace std;
+int dp[1001][1001] = { 0, };
 
 int main(){
-    cout << "이름_문제번호 형태로 파일 제출하기 (예시: 승운_2977)";
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+
+    for(int i=1; i<=s1.size(); i++){ // LIS과 구도가 유사하다.
+        for(int j=1; j<=s2.size(); j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = max(dp[i-1][j-1] + 1, dp[i][j]);
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    cout << dp[s1.size()][s2.size()];
+    return 0;
 }
