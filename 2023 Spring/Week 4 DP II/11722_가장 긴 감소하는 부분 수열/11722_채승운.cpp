2ab57501d7b6c1f7b94c45531f95d6c5aa11fb16
CHAE <chaso1495@naver.com>
2023-03-27 23:28:01 +0900

    [feat] 11722 가장 긴 감소하는 부분 수열, 14728 벼락치기

--- 2023 Spring/Week 4 DP II/11722_가장 긴 감소하는 부분 수열/11722_채승운.cpp
+++ 2023 Spring/Week 4 DP II/11722_가장 긴 감소하는 부분 수열/11722_채승운.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
 using namespace std;
+int n;
+int arr[1001];
+int dp[1001];
 
 int main(){
-    cout << "이름_문제번호 형태로 파일 제출하기 (예시: 승운_2977)";
+    int res = 1;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> arr[i];
+    } // 입력
+
+    dp[1] = 1;
+    for(int i=2; i<=n; i++){
+        dp[i] = 1; // 매 dp마다 초기화
+        for(int j=1; j<i; j++){
+            if(arr[i] < arr[j]){
+                dp[i] = max(dp[j] + 1, dp[i]);
+            }
+        }
+        res = max(res, dp[i]); // 갱신
+    }
+    cout << res << '\n';
+    return 0;
 }
--- 2023 Spring/Week 4 DP II/17208_카우버거 알바생/17208_채승운.cpp
+++ 2023 Spring/Week 4 DP II/17208_카우버거 알바생/17208_채승운.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+int n, k, w, v;
+int dp[101][100001];
+vector<pair<int, int> > vec;
 
 int main(){
-    cout << "이름_문제번호 형태로 파일 제출하기 (예시: 승운_2977)";
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        cin >> w >> v;
+        vec.push_back(make_pair(w, v));
+    } // 무게와 가치 입력
+
+    for(int i=1; i<=n; i++){
+        int weight = vec[i-1].first;
+        int val = vec[i-1].second;
+    
+        for(int j=0; j<=k; j++){
+            if(weight <= j){
+                dp[i][j] = max(dp[i-1][j-weight] + val, dp[i-1][j]);
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+    cout << dp[n][k] << '\n';
+    return 0;
 }
